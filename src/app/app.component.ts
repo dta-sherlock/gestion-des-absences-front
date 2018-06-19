e47944f53f49b35b1811c26631ca2c962e565ede
Quentin <quent.wojtasiak@hotmail.com>
2018-06-19 09:58:45 +0200

    Creation du layout (menu)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
@@ -7,5 +6,4 @@ import {environment} from "../environments/environment";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  apiUrl = environment.apiUrl;
 }

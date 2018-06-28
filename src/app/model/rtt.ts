e3fb6c7effcc3555ec938e275766ce5571fb24fd
Geraud Tourrilhes <geraud.tourrilhes@gmail.com>
2018-06-28 16:52:22 +0200

    ajout rtt Employeur et ferie

--- src/app/model/rtt.ts
+++ src/app/model/rtt.ts
@@ -1,7 +1,8 @@
 import {FerieRtt} from './ferie_rtt';
+import {ferieRttType} from './EnumFerieRttType';
 
 export class Rtt extends FerieRtt {
   constructor(public id: number, public date: Date, public commentaire: string, public statut: string) {
-    super(id, date, 'RTT employeur', commentaire);
+    super(id, date, ferieRttType.RTT, commentaire);
   }
 }
